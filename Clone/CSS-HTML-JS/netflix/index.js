9f1bdf360470e00b43216f34b8bf7df125c9ee06
DanilV <dani736@gmail.com>
2023-04-22 12:33:47 +0300

    update styles

--- Clone/CSS-HTML-JS/netflix/index.js
+++ Clone/CSS-HTML-JS/netflix/index.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector(".nav");
 window.onscroll = function () {
-  var top = window.scrollY;
+  let top = window.scrollY;
   if (top >= 30) {
     nav.classList.add("active");
   } else {
