78e0f56688798966aed03983177a30299fac9c15
DanilVo <Dani736@gmail.com>
2023-04-30 11:19:51 +0300

    fix finished todo

--- To-do App/main.js
+++ To-do App/main.js
@@ -1,39 +1,57 @@
-
-const userInput = document.querySelector(".user-input");
-const mainList = document.querySelector("ul")
-
-const newListItem = document.createElement('li')
-const newSpan = document.createElement('span')
-const deleteButton = document.createElement('button')
-deleteButton.textContent = "Delete"
-const editButton = document.createElement('button')
-editButton.textContent = "Edit"
+const userInput = document.querySelector('.user-input');
+const mainList = document.querySelector('ul');
 
 function addTodo() {
+  const newListItem = document.createElement('li');
+  const deleteButton = document.createElement('button');
+  const newSpan = document.createElement('input');
+  deleteButton.textContent = 'Delete';
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  newSpan.setAttribute('readonly', 'readonly');
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
 
-    const inputValue = userInput.value
-    
-    if (inputValue.length > 0) {
-        mainList.appendChild(newListItem)
-        newSpan.textContent = inputValue;
-        newListItem.appendChild(newSpan)
-        newListItem.appendChild(deleteButton);
-        newListItem.appendChild(editButton);
+  const inputValue = userInput.value;
+
+  if (inputValue.length > 0) {
+    mainList.appendChild(newListItem);
+    newListItem.appendChild(checkBox);
+    newSpan.value = inputValue;
+    newListItem.appendChild(newSpan);
+    newListItem.append(deleteButton);
+    newListItem.appendChild(editButton);
+  } else {
+    alert('Please enter valid To-do');
+  }
+
+  userInput.value = '';
+
+  userInput.focus();
+  deleteButton.addEventListener('click', () =>
+    mainList.removeChild(newListItem)
+  );
+
+  editButton.addEventListener('click', editTodo);
+
+  function editTodo() {
+    if (editButton.textContent == 'Edit') {
+      newSpan.removeAttribute('readonly', 'readonly');
+      editButton.textContent = 'Save';
+      newSpan.focus();
     } else {
-        alert("Please enter valid To-do")
+      newSpan.setAttribute('readonly', 'readonly');
+      editButton.textContent = 'Edit';
     }
-    
-    userInput.value = ''
-    
-    deleteButton.addEventListener('click', () => mainList.removeChild(newListItem))
-    editButton.addEventListener('click', editTodo())
-    
-    userInput.focus()
-}
+  }
 
-function editTodo() {
-    const editSpan = document.querySelector('ul').children[0]
-    const newInput = document.createElement('input') 
+  checkBox.addEventListener('click', finishedTodo);
 
-    mainList.replaceChild(newInput ,editSpan.childNodes[0])
+  function finishedTodo() {
+    if(checkBox.checked == false) {
+      checkBox.classList.add('checked')
+    } else {
+      checkBox.classList.remove('checked')
+    }
+  }
 }
